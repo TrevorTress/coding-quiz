82423596691c6a6d2065d53041f66880fd5a8a15
TrevorTress <trevor.tress@outlook.com>
2022-03-06 14:46:19 -0500

    quiz finalized for grade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,35 @@
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
+var submitButton = document.getElementById('submit-btn')
 var questionContainerEl = document.getElementById('question-container')
 var questionEl = document.getElementById('question')
 var answerButtonsEl = document.getElementById('answer-buttons')
+var submitContainerEl = document.getElementById('submit-box')
+var usernameEl = document.getElementById('name-input')
+var saveButtonEl = document.getElementById('save-btn')
+var leaderboardEl = document.getElementById('leaderboard')
+
+const startingMins = 1
+let time = startingMins * 60
+var countdownEl = document.getElementById('countdown')
+
+let points = 0
+const highScores = JSON.parse(localStorage.getItem('highscores')) || []
+
+function updateCountdown() {
+    const minutes = Math.floor(time/60)
+    let seconds = time % 60
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    if (time >= 0) {
+        countdownEl.innerHTML = `${minutes}:${seconds}`
+        time --
+    } else {
+        clearInterval(updateCountdown)
+        submitButton.classList.remove('hide')
+    }
+    
+}
 
 // click start = start game
 startButton.addEventListener('click', startGame)
@@ -12,6 +39,7 @@ nextButton.addEventListener('click', () => {
 })
 
 function startGame() {
+    setInterval(updateCountdown, 1000)
     // hide the start button
     startButton.classList.add('hide')
 
@@ -62,23 +90,27 @@ function resetQuestion() {
     }
 }
 
-function selectAnswer(event) {
-    // id what was clicked and assign value of correct to it
+// id what was clicked and assign value of correct to it
+function selectAnswer(event) {    
     var selectedButton = event.target
     var correct = selectedButton.dataset.correct
     setStatusClass(document.body, correct)
     Array.from(answerButtonsEl.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (questionOrder.length > currentQuestionIndex + 1) {
+    if (questionOrder.length > currentQuestionIndex + 1 && time >= 0) {
         nextButton.classList.remove('hide')
     } else {
-        startButton.innerText = 'Restart?'
-        startButton.classList.remove('hide')
+        submitButton.classList.remove('hide')
+        clearInterval(updateCountdown)
     }
-    
+    if (correct) {
+        points += 10
+        console.log(points)
+    } else {time-10}
 }
 
+// adds correct/wrong classes to buttons
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
@@ -88,50 +120,116 @@ function setStatusClass(element, correct) {
     }
 }
 
+// resets correct/wrong between questions
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
 
+submitButton.addEventListener('click', submitScore)
+
+function submitScore() {
+    submitContainerEl.classList.remove('hide')
+    clearInterval(updateCountdown)
+}
+
+saveButtonEl.addEventListener('click', saveScore)
+
+function saveScore() {
+    var submittedScore = 
+    {
+        score: points,
+        name: usernameEl.value
+    }
+
+    highScores.push(submittedScore)
+    localStorage.setItem('submittedScore', JSON.stringify(submittedScore))
+    
+    var leaderboardEntry = document.createElement('li')
+    leaderboardEntry.className = 'leaderboard-entry'
+    leaderboardEntry.innerHTML = JSON.stringify(submittedScore)
+}
+
+
+
 // array of question objects with answers
 var questions = [
     {
-        question: 'The answer to this is 1',
+        question: 'What language makes up the foundation of all web pages?',
         answers: [
-            {text: '1', correct: true},
-            {text: '2', correct: false},
-            {text: '3', correct: false},
-            {text: '4', correct: false}
+            {text: 'HTML', correct: true},
+            {text: 'CSS', correct: false},
+            {text: 'Javascript', correct: false},
+            {text: 'Spanish', correct: false}
         ]
     },
 
     {
-        question: 'The answer to this is 2',
+        question: 'What language is used primarily for styling a webpage?',
         answers: [
-            {text: '1', correct: false},
-            {text: '2', correct: true},
-            {text: '3', correct: false},
-            {text: '4', correct: false}
+            {text: 'HTML', correct: false},
+            {text: 'CSS', correct: true},
+            {text: 'Javascript', correct: false},
+            {text: 'Klingon', correct: false}
         ]
     },
 
     {
-        question: 'The answer to this is 3',
+        question: 'What language is used primarily for making a web page do things?',
         answers: [
-            {text: '1', correct: false},
-            {text: '2', correct: false},
-            {text: '3', correct: true},
-            {text: '4', correct: false}
+            {text: 'HTML', correct: false},
+            {text: 'CSS', correct: false},
+            {text: 'Javascript', correct: true},
+            {text: 'Elvish', correct: false}
         ]
     },
 
     {
-        question: 'The answer to this is 4',
+        question: 'What was Harry Potters second language?',
         answers: [
-            {text: '1', correct: false},
-            {text: '2', correct: false},
-            {text: '3', correct: false},
-            {text: '4', correct: true}
+            {text: 'HTML', correct: false},
+            {text: 'CSS', correct: false},
+            {text: 'Javascript', correct: false},
+            {text: 'Parseltongue', correct: true}
+        ]
+    },
+    
+    {
+        question: 'TEST QUESTION 1',
+        answers: [
+            {text: 'INCORRECT', correct: false},
+            {text: 'INCORRECT', correct: false},
+            {text: 'CORRECT', correct: true},
+            {text: 'INCORRECT', correct: false}
+        ]
+    },
+
+    {
+        question: 'TEST QUESTION 2',
+        answers: [
+            {text: 'CORRECT', correct: true},
+            {text: 'INCORRECT', correct: false},
+            {text: 'INCORRECT', correct: false},
+            {text: 'INCORRECT', correct: false}
+        ]
+    },
+
+    {
+        question: 'TEST QUESTION 3',
+            answers: [
+                {text: 'INCORRECT', correct: false},
+                {text: 'INCORRECT', correct: false},
+                {text: 'INCORRECT', correct: false},
+                {text: 'CORRECT', correct: true}
         ]
     }
 ]
+
+/*
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+*/
